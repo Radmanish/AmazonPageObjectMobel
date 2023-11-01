8edcfa8451787a7bc85ba9e690675d32a20b5cb4
Hamidbabak42 <Hamidbabak42@gmail.com>
2023-10-31 22:43:32 -0500

    Hamid

--- src/main/java/in/amazon/pages/BuyAppleMobile.java
+++ src/main/java/in/amazon/pages/BuyAppleMobile.java
@@ -1,5 +1,16 @@
 package in.amazon.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+
 public class BuyAppleMobile {
 
+	
+	Actions action;
+	public LandingPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+		action = new Actions (driver);
+		
+}
 }
