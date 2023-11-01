3e594651250adf10f8e2e953ab42852a66af7f1d
unknown <sayedmeyapadshah@gmail.com>
2023-10-31 20:47:45 -0700

    new update

--- src/main/java/in/amazon/pages/SignInPage.java
+++ src/main/java/in/amazon/pages/SignInPage.java
@@ -1,5 +1,13 @@
 package in.amazon.pages;
 
-public class SignInPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
+public class SignInPage {
+	
+	public SignInPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+		
+		
+	}
 }
